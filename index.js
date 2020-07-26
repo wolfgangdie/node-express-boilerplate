301480b0970a6b132f5728a24a25682a97edd8c8
Wolfgang Dielingen <wolfrainx@gmail.com>
2020-07-26 02:27:05 -0400

    Adding middlewares for parsing request body

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router);
 
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) =>
   res.status(200).json({ message: 'Node + Express (API)' })
 );
 
+router.post('/', (req, res) =>
+  res.status(200).json({ message: 'Node + Express (API)', body: req.body })
+);
+
 module.exports = router;
